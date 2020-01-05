7b0a95a243ab5773958444f55c376abe88f4dc6a
benborgers <borgersbenjamin@gmail.com>
2020-01-05 15:02:51 -0500

    add a couple meta properties

--- src/components/Head.js
+++ src/components/Head.js
@@ -10,9 +10,12 @@ export default () => {
       <title>{title}</title>
       <meta name="description" content={description} />
 
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content="https://figure.netlify.com/progress-share-image" />
+
+      <meta property="twitter:card" content="summary_large_image" />
 
       <link rel="icon" href="https://emojicdn.elk.sh/🌖" />
     </Helmet>
