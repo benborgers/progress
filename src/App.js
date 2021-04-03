c4f6b63576ed9a7b2786568881231e8b7a89fb00
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-03 19:07:59 -0400

    Remove references to deleted components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 //
 import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
 
 import './app.css'
 
