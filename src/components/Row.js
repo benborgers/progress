2505a348e02d36e86ba6214c72fa7206e901f1c9
Ben Borgers <borgersbenjamin@gmail.com>
2021-04-03 21:29:31 -0400

    Transition in rows

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
-export default function Row({ label, number = '', color }) {
+export default function Row({ label, number = '', color, variants=null }) {
     return (
-        <div className="lg:flex items-end justify-between lg:space-x-16 space-y-2 lg:space-y-0">
+        <motion.div
+            className="lg:flex items-end justify-between lg:space-x-16 space-y-2 lg:space-y-0"
+            variants={variants}
+        >
             <p className={`${color} text-gray-300 text-3xl sm:text-4xl font-serif`}>{label}</p>
 
-            <p className="text-gray-100 text-3xl sm:text-5xl font-black tabular-nums">
+            <p className="text-gray-100 text-4xl sm:text-5xl font-black tabular-nums">
                 {number}
             </p>
-        </div>
+        </motion.div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,23 @@ import useCalculation from '../hooks/useCalculation'
 export default function Index() {
     const calculation = useCalculation()
 
+    const container = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                delay: 0.5,
+                delayChildren: 0.5,
+                staggerChildren: 0.5
+            }
+        }
+    }
+
+    const item = {
+        hidden: { opacity: 0, y: 8 },
+        show: { opacity: 1, y: 0 }
+    }
+
     return (
         <div className="p-4 sm:p-8">
             <motion.div
@@ -14,15 +31,20 @@ export default function Index() {
                 initial={{ y: -50, opacity: 0 }}
                 animate={{ y: 0, opacity: 1  }}
             >
-                <p className="font-extrabold text-3xl">LHS Senior Countdown</p>
-                <p className="font-bold flex items-center space-x-1 italic">our last day is {calculation.END.format('MMM    D')}</p>
+                <p className="font-extrabold text-2xl sm:text-3xl">LHS Senior Countdown</p>
+                <p className="text-gray-500 font-semibold flex items-center space-x-1 italic">our last day is {calculation.END.format('MMM    D')}</p>
             </motion.div>
 
-            <div className="space-y-12 lg:space-y-8">
-                <Row label="School days left" number={calculation.daysLeft} color="text-yellow-400" />
-                <Row label="Weeks of school left" number={calculation.weeksLeft} color="text-orange-400" />
-                <Row label="Hours until the end of the school year" number={calculation.minutesLeft} color="text-red-400" />
-            </div>
+            <motion.div
+                className="space-y-12"
+                variants={container}
+                initial="hidden"
+                animate="show"
+            >
+                <Row label="School days left" number={calculation.daysLeft} color="text-yellow-400" variants={item} />
+                <Row label="Weeks of school left" number={calculation.weeksLeft} color="text-orange-400" variants={item} />
+                <Row label="Hours until the end of the school year" number={calculation.minutesLeft} color="text-red-400" variants={item} />
+            </motion.div>
         </div>
     )
 }
